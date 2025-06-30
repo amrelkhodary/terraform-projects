0c9772b22633508c09541f4b482a2d45a403c9d9
amrelkhodary <amrelkhodarybusiness@gmail.com>
2025-06-30 12:58:03 +0300

    edited group.tf so that IAM policies are not represented as data blocks

--- terraform-iam/group.tf
+++ terraform-iam/group.tf
@@ -1,58 +1,37 @@
-#submodule to create a new group and attach IAM policies to it
+# Submodule to create a new group and attach IAM policies to it
 
-#fetching required IAM policies
-data "aws_iam_policy" "vpc_full_access" {
-  arn = "arn:aws:iam::aws:policy/AmazonVPCFullAccess"
-}
-
-data "aws_iam_policy" "ec2_full_access" {
-  arn = "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
-}
-
-data "aws_iam_policy" "iam_readonly" {
-  arn = "arn:aws:iam::aws:policy/IAMReadOnlyAccess"
-}
-
-data "aws_iam_policy" "elb_full_access" {
-  arn = "arn:aws:iam::aws:policy/ElasticLoadBalancingFullAccess"
-}
-
-data "aws_iam_policy" "autoscaling_full_access" {
-  arn = "arn:aws:iam::aws:policy/AutoScalingFullAccess"
-}
-
-#creating a new group (might return an error if the group already exists)
+# Creating a new group (might return an error if the group already exists)
 resource "aws_iam_group" "users" {
     name = var.group-name 
 }
 
-#attaching the policies to the group
+# Attaching the policies to the group using direct ARN references
 resource "aws_iam_policy_attachment" "group_vpc_full_access" {
-    name = "group-vpc-full-access-attachment"
-    policy_arn = data.aws_iam_policy.vpc_full_access.arn
-    groups = [aws_iam_group.users.name]
+    name       = "group-vpc-full-access-attachment"
+    policy_arn = "arn:aws:iam::aws:policy/AmazonVPCFullAccess"
+    groups     = [aws_iam_group.users.name]
 }
 
 resource "aws_iam_policy_attachment" "group_ec2_full_access" {
-    name = "group-ec2-full-access-attachment"
-    policy_arn = data.aws_iam_policy.ec2_full_access.arn
-    groups = [aws_iam_group.users.name]
+    name       = "group-ec2-full-access-attachment"
+    policy_arn = "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
+    groups     = [aws_iam_group.users.name]
 }
 
 resource "aws_iam_policy_attachment" "group_iam_read_only" {
-    name = "group-iam-read-only-attachment"
-    policy_arn = data.aws_iam_policy.iam_readonly.arn
-    groups = [aws_iam_group.users.name]
+    name       = "group-iam-read-only-attachment"
+    policy_arn = "arn:aws:iam::aws:policy/IAMReadOnlyAccess"
+    groups     = [aws_iam_group.users.name]
 }
 
 resource "aws_iam_policy_attachment" "group_elb_full_access" {
-    name = "group-elb-full-access-attachment"
-    policy_arn = data.aws_iam_policy.elb_full_access.arn
-    groups = [aws_iam_group.users.name]
+    name       = "group-elb-full-access-attachment"
+    policy_arn = "arn:aws:iam::aws:policy/ElasticLoadBalancingFullAccess"
+    groups     = [aws_iam_group.users.name]
 }
 
 resource "aws_iam_policy_attachment" "group_asg_full_access" {
-    name = "group-asg-full-access-attachment"
-    policy_arn = data.aws_iam_policy.autoscaling_full_access.arn
-    groups = [aws_iam_group.users.name]
+    name       = "group-asg-full-access-attachment"
+    policy_arn = "arn:aws:iam::aws:policy/AutoScalingFullAccess"
+    groups     = [aws_iam_group.users.name]
 }
